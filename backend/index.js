5ddae2d88aeeda803741fe1d468a48b977a7d213
heyvji <vetrivijay1003@gmail.com>
2026-01-14 21:18:46 +0530

    1.02.01

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,9 @@ app.post("/api/contact", async (req, res) => {
   }
 })
 
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running");
 });
+
